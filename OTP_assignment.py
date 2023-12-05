0421e23271cba20c2e1decf7500e46719286815b
Suyog Naik <naiksuyogsudhir@gmail.com>
2023-12-06 01:39:49 +0530

    OOP Version3

--- OTP_assignment.py
+++ OTP_assignment.py
@@ -3,27 +3,23 @@ import random
 import re
 from twilio.rest import Client
 
-class OTPService:
-    """
-    Class docstring: OTPService class handles the generation and sending of OTPs.
-    """
+class CommunicationService:
+    @staticmethod
+    def generate_otp(length=6):
+        digits = "0123456789"
+        return ''.join(random.choice(digits) for _ in range(length))
 
+    def send_otp(self, identifier, otp):
+        raise NotImplementedError("Subclasses must implement this method.")
+
+class OTPService(CommunicationService):
     def __init__(self, account_sid, auth_token, twilio_number):
+        super().__init__()
         self.account_sid = account_sid
         self.auth_token = auth_token
         self.twilio_number = twilio_number
 
-    def generate_otp(self):
-        """
-        Generates a 6-digit OTP.
-        """
-        digits = "0123456789"
-        return ''.join(random.choice(digits) for _ in range(6))
-
     def send_email(self, email, otp):
-        """
-        Sends the OTP via email.
-        """
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
         server.login('naiksuyogsudhir@gmail.com', 'kajzazcwvvnbhgxg')
@@ -32,9 +28,6 @@ class OTPService:
         server.quit()
 
     def send_otp_over_mobile(self, mobile_no, otp):
-        """
-        Sends the OTP via Twilio SMS.
-        """
         client = Client(self.account_sid, self.auth_token)
         message_body = f'Your 6 digit OTP is {otp}'
         message = client.messages.create(
@@ -45,46 +38,35 @@ class OTPService:
         print(message.body)
 
     def send_otp_to_mobile_user(self, mobile_user):
-        """
-        Sends OTP to a MobileUser instance.
-        """
-        if mobile_user.validate_mobile_no():
-            self.send_otp_over_mobile(mobile_user.mobile_no, self.generate_otp())
+        if mobile_user.validate():
+            self.send_otp_over_mobile(mobile_user.get_identifier(), self.generate_otp())
         else:
             print("Invalid Mobile number")
 
     def send_otp_to_email_user(self, email_user):
-        """
-        Sends OTP to an EmailUser instance.
-        """
-        if email_user.validate_email():
-            self.send_email(email_user.email, self.generate_otp())
+        if email_user.validate():
+            self.send_email(email_user.get_identifier(), self.generate_otp())
         else:
             print("Invalid Email")
 
+class User:
+    def __init__(self, identifier):
+        self.identifier = identifier
 
-class MobileUser:
-    def __init__(self, mobile_no):
-        self.mobile_no = mobile_no
+class MobileUser(User):
+    def validate(self):
+        return len(self.identifier) == 10 and self.identifier.isdigit()
 
-    def validate_mobile_no(self):
-        """
-        Validates a mobile number.
-        """
-        return len(self.mobile_no) == 10 and self.mobile_no.isdigit()
+    def get_identifier(self):
+        return self.identifier
 
-
-class EmailUser:
-    def __init__(self, email):
-        self.email = email
-
-    def validate_email(self):
-        """
-        Validates an email address.
-        """
+class EmailUser(User):
+    def validate(self):
         validation_condition = r"^[\w\.-]+@[\w\.-]+\.\w+$"
-        return bool(re.search(validation_condition, self.email))
+        return bool(re.search(validation_condition, self.identifier))
 
+    def get_identifier(self):
+        return self.identifier
 
 if __name__ == "__main__":
     ACCOUNT_SID = "AC74d3234c1b27da3f53a19340e214c05f"
